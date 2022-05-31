f41d94491e672bf6c652aab6f1c0d812763ce704
Josh Arnold <jarnold@ucdavis.edu>
2022-05-30 20:24:34 -0700

    Initial commit

---
+++ ActionSheet.swift
@@ -0,0 +1,167 @@
+//
+//  ActionSheet.swift
+//  Action Sheet Demo
+//
+//  Created by Josh Arnold on 5/30/22.
+//
+
+import SwiftUI
+
+// MARK: - View
+
+struct ActionSheet<Content: View>: View {
+    
+    // MARK: - Constants
+    
+    private let dimBackgroundAmount = 0.5
+    
+    private let requiredDismissVelocity: CGFloat = 6
+    
+    private let fadeDimBackgroundTolerance: CGFloat = 120
+
+    // MARK: - Properties
+    
+    @Binding var isPresented: Bool
+    
+    @ViewBuilder let content: Content
+    
+    @State private var dimOpacity: Double = 0.0
+
+    @State private var dragOffset: Double = UIScreen.main.bounds.height
+    
+    // MARK: - Body
+    
+    var body: some View {
+        GeometryReader { proxy in
+            VStack {
+                Spacer()
+                VStack {
+                    Divider()
+                    content
+                    Spacer()
+                        .frame(height: max(-dragOffset, .zero)) // Allows for "stretching the view
+                }
+                .background(.background)
+                .offset(x: .zero, y: max(dragOffset, .zero)) // Allows for dragging to dismiss
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        dragOffset = value.translation.height
+                        dimOpacity = dragOffset > 0 ? dimBackgroundAmount * (1 - (dragOffset / fadeDimBackgroundTolerance)) : dimBackgroundAmount
+                    })
+                    .onEnded({ value in
+                        let velocity = value.predictedEndLocation.y - value.location.y
+                        if velocity > requiredDismissVelocity {
+                            dismissActionSheet(actionSheetHeight: proxy.size.height)
+                        } else {
+                            resetActionSheet()
+                        }
+                    })
+            )
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
+                Rectangle()
+                    .background(Color.black)
+                    .opacity(dimOpacity)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        dismissActionSheet(actionSheetHeight: proxy.size.height)
+                    }
+            )
+            .onAppear {
+                if isPresented {
+                    showActionSheet(actionSheetHeight: proxy.size.height)
+                } else {
+                    dismissActionSheet(actionSheetHeight: proxy.size.height, animated: false)
+                }
+            }
+            .onChange(of: isPresented) { newValue in
+                if newValue {
+                    showActionSheet(actionSheetHeight: proxy.size.height)
+                } else {
+                    dismissActionSheet(actionSheetHeight: proxy.size.height)
+                }
+            }
+        }
+    }
+    
+    private func test(val: CGFloat) {
+        print(val)
+    }
+    
+    // MARK: - Helper
+    
+    /// Presents the action sheet
+    private func showActionSheet(actionSheetHeight: Double) {
+        withAnimation(.easeOut) {
+            dragOffset = .zero
+            dimOpacity = dimBackgroundAmount
+        }
+    }
+    
+    /// Hides the action sheet
+    private func dismissActionSheet(actionSheetHeight: Double, animated: Bool = true) {
+        withAnimation(animated ? .easeIn : .none) {
+            dragOffset = actionSheetHeight
+            dimOpacity = .zero
+            isPresented = false
+        }
+    }
+    
+    /// Resets the action sheet back to its normal, visible position
+    private func resetActionSheet() {
+        withAnimation(.spring()) {
+            dragOffset = 0.0
+            dimOpacity = dimBackgroundAmount
+        }
+    }
+}
+
+// MARK: - View Modifier
+
+struct ActionSheetModifier<V: View>: ViewModifier {
+    
+    @Binding var isPresented: Bool
+    
+    @ViewBuilder var actionSheetContent: V
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            content.allowsHitTesting(!isPresented)
+            ActionSheet(isPresented: $isPresented) {
+                actionSheetContent
+            }
+            .allowsHitTesting(isPresented)
+        }
+    }
+}
+
+extension View {
+    func actionSheet<V: View>(
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: () -> V
+    ) -> some View {
+        modifier(
+            ActionSheetModifier(
+                isPresented: isPresented,
+                actionSheetContent: {
+                    content()
+                }
+            )
+        )
+    }
+}
+
+// MARK: - Previews
+
+struct ActionSheet_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ActionSheet(isPresented: .constant(true)) {
+            Image(systemName: "questionmark")
+                .fixedSize()
+                .frame(height: 200)
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,124 @@
 # SwiftUI-Action-Sheet-Example
+
 An example of how to build a reusable action sheet in SwiftUI.
+
+![An example of the action sheet.](img/example.gif)
+
+## Usage
+
+```swift
+var body: some View {
+    content
+        .actionSheet(isPresented: $isPresented) { // Presents an action sheet when `isPresented` is set to true
+            Text("I'm an action sheet!")
+        }
+}
+```
+
+## How does it work?
+
+Check `ActionSheet.swift` for the source code, although here is a brief rundown of the core ideas below:
+
+### 1. First, we use a `ViewModifier` to be able to wrap Views
+
+The `.actionSheet` function you can call on any SwiftUI `View` is built using a `ViewModifier`. It is defined like so:
+
+```swift
+extension View {
+    // Define the action sheet function that can be called on any `View`
+    func actionSheet(...) -> some View {
+        modifier(ActionSheetModifier(...)
+    }
+}
+
+struct ActionSheetModifier: ViewModifier {    
+    // Our actual view modifier
+    // The `content` argument is the `View` that the `actionSheet` function is being called on
+    // We can manipulate the `View` / `content` however we want!
+    func body(content: Content) -> some View {
+        content
+    }
+}
+```
+
+At a high-level, our `ViewModifier` essentially wraps the `content` in a `ZStack` with our `ActionSheet`:
+
+```swift
+struct ActionSheetModifier: ViewModifier {
+
+    @Binding var actionSheetIsPresented: Bool
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            if actionSheetIsPresented {
+                ActionSheet()
+            } else {
+                EmptyView()
+            }
+        }
+    }
+}
+```
+
+The code above is simplified, but as you can see, `ViewModifiers` are super powerful because we can wrap/encapsulate any view with another, simply by calling a function on a `View`.
+
+
+### 2. The second idea, is we allow our view modifier to take a `@ViewBuilder`
+
+Without getting into the weeds of it, the `@ViewBuilder` decorator allows our functions to take SwiftUI closures that return `some View` as an argument.
+
+For example, if you see the `actionSheet` function below, there is a closure where you can build a SwiftUI `View`, which will be used to populate the Action Sheet.
+
+```swift
+.actionSheet(isPresented: $isPresented) {
+    VStack {
+        Text("I'm an action sheet!")
+    }
+}
+```
+
+This is a super cool feature of SwiftUI.
+
+The **trick** to working with `@ViewBuilder` I've found, **is to use Generics**. In otherwords, when writing your functions and classes that use `@ViewBuilder`, add a generic constraint: `<V: View>`:
+
+```swift
+// 1. We return a generic `View`, `V` in our `@ViewBuilder` closure
+func actionSheet<V: View>(isPresented: Binding<Bool>, @ViewBuilder content: () -> V)
+
+// 2. We use a generic `View`, `V` in our `ViewModifier` struct
+struct ActionSheetModifier<V: View>: ViewModifier {
+
+    @ViewBuilder var actionSheetContent: V
+    
+    func body(content: Content) -> some View {
+        // ...
+        ActionSheet {
+            actionSheetContent
+        }
+    }    
+}
+
+// 3. The ActionSheet SwiftUI `View` itself has a generic constraint too that allows us to use `@ViewBuilder` to take a SwiftUI `View` as an input argument.
+struct ActionSheet<Content: View>: View {
+
+    // ...
+
+    @ViewBuilder let content: Content
+
+    // ...
+}
+```
+
+### 3. Using `DragGesture()` and `Binding`
+
+The last idea here, is that we used `DragGesture()` to handle panning the Action Sheet vertically. 
+
+Using `@Binding` also provided us with a bi-directional data flow, allowing our Action Sheet struct to dismiss itself and set `isPresented = false`, and also allow the `View` that calls the `.actionSheet` function to toggle `isPresented` as well.
+
+There are many great tutorials on gestures and bindings in Swift, so I'd recommend checking those out for more clarification.
+
+## Conclusion
+
+I hope this example helps! Feel free to check out my website at https://josharnold.me.
+
